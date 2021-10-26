a868e6cf2e4c13190958078e72cfa3d80bfe8fa8
Asmatchd <Asmatchd@gmail.com>
2021-10-26 13:47:17 +0500

    touchable opacity done, signupscreen designed

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Basics} from './src/screens/basics';
 import {BasicsTwo} from './src/screens/basicsTwo';
+import {SignUp} from './src/screens/signUp';
 
 const App = () => {
-  return <BasicsTwo />;
+  return <SignUp />;
 };
 
 export default App;
---
+++ src/screens/signUp/SignUp.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+export class SignUp extends React.Component {
+  render() {
+    return (
+      <KeyboardAwareScrollView
+        contentContainerStyle={{
+          flexGrow: 2,
+        }}>
+        <View
+          style={{
+            flex: 1,
+            // backgroundColor: '#aaf',
+          }}>
+          {/* spacer */}
+          <View
+            style={{
+              height: '15%',
+              //   backgroundColor: '#faf',
+            }}
+          />
+          {/* top view */}
+          <View
+            style={{
+              //   backgroundColor: '#a4a',
+              paddingLeft: 15,
+            }}>
+            <Text
+              style={{
+                fontSize: 25,
+                fontWeight: 'bold',
+                color: '#000',
+              }}>
+              Welcome
+            </Text>
+
+            <Text
+              style={{
+                fontSize: 18,
+                color: '#000',
+              }}>
+              glad to see you!
+            </Text>
+          </View>
+
+          {/* spacer */}
+          <View
+            style={{
+              height: '10%',
+              //   backgroundColor: '#faf',
+            }}
+          />
+
+          {/* bottom view */}
+          <View
+            style={{
+              //   backgroundColor: '#1af',
+              height: '50%',
+              padding: 15,
+            }}>
+            <TextInput
+              style={{
+                // backgroundColor: '#aaf',
+                borderColor: 'red',
+                borderBottomWidth: 0.5,
+              }}
+              placeholder={'Name'}
+            />
+
+            <TextInput
+              style={{
+                // backgroundColor: '#aaf',
+                borderColor: 'red',
+                borderBottomWidth: 0.5,
+                marginTop: 10,
+              }}
+              placeholder={'Email'}
+            />
+
+            <TextInput
+              style={{
+                // backgroundColor: '#aaf',
+                borderColor: 'red',
+                borderBottomWidth: 0.5,
+                marginTop: 10,
+              }}
+              placeholder={'Password'}
+            />
+
+            <View
+              style={{
+                // backgroundColor: '#faf',
+                marginTop: 40,
+                width: '100%',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#000',
+                  height: 50,
+                  width: '70%',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 7,
+                }}
+                // disabled
+              >
+                <Text
+                  style={{
+                    color: '#fff',
+                  }}>
+                  Create Account
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+    );
+  }
+}
