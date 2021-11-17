cdd69216d8821a4fbeda4ae88f215ef0d288e63c
Asmatchd <Asmatchd@gmail.com>
2021-11-17 14:22:21 +0500

    working oin flatlist

--- src/screens/list/List.js
+++ src/screens/list/List.js
@@ -8,8 +8,169 @@ import {
 
 export class List extends React.Component {
   state = {
-    data: [],
+    data: [
+      {
+        name: 'Ali',
+        fName: 'Umer',
+        phone: '123456789',
+        age: '27',
+        img: require('../../assets/1.jpg'),
+      },
+      {
+        name: 'Imran',
+        fName: 'Kashif',
+        phone: '123450987',
+        age: '37',
+        img: require('../../assets/2.jpg'),
+      },
+
+      {
+        name: 'kashif',
+        fName: 'Umer',
+        phone: '5678432',
+        age: '20',
+        img: require('../../assets/3.jpg'),
+      },
+      {
+        name: 'ali',
+        fName: 'Kashif',
+        phone: '9876543',
+        age: '16',
+        img: require('../../assets/4.jpg'),
+      },
+    ],
+    loggedIn: true,
   };
+
+  /*   renderDesign = item => {
+    if (this.state.loggedIn) {
+      return (
+        <View
+          style={{
+            height: h('8%'),
+            // backgroundColor: '#aaf',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            flexDirection: 'row',
+            borderWidth: h('0.1%'),
+            borderRadius: h('1%'),
+          }}>
+          <Text
+            style={{
+              fontSize: h('3%'),
+            }}>
+            Name: {item.name}
+          </Text>
+          <Text>Father Name: {item.fName}</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View
+          style={{
+            height: h('8%'),
+            // backgroundColor: '#aaf',
+            alignItems: 'center',
+            justifyContent: 'space-evenly',
+            flexDirection: 'row',
+            borderWidth: h('0.1%'),
+            borderRadius: h('1%'),
+          }}>
+          <Text
+            style={{
+              fontSize: h('3%'),
+            }}>
+            No data
+          </Text>
+        </View>
+      );
+    }
+  }; */
+
+  renderDesign = item => (
+    <View
+      style={{
+        height: h('11%'),
+        // backgroundColor: '#aaf',
+        flexDirection: 'row',
+        borderWidth: h('0.1%'),
+        borderRadius: h('1%'),
+        borderColor: 'grey',
+      }}>
+      <View
+        style={{
+          height: '100%',
+          width: '20%',
+          // backgroundColor: '#faf',
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'hidden',
+        }}>
+        <Image
+          source={item.img}
+          style={{
+            height: h('10%'),
+            width: h('10%'),
+            resizeMode: 'cover',
+            borderRadius: h('5%'),
+          }}
+        />
+      </View>
+
+      <View
+        style={{
+          height: '100%',
+          width: '80%',
+          // backgroundColor: '#a3a',
+        }}>
+        <View
+          style={{
+            height: '50%',
+            width: '100%',
+            // backgroundColor: '#af3',
+            flexDirection: 'row',
+          }}>
+          <View
+            style={{
+              height: '100%',
+              width: '70%',
+              // backgroundColor: 'red',
+              justifyContent: 'center',
+              paddingLeft: h('2%'),
+            }}>
+            <Text>Name: {item.name}</Text>
+          </View>
+          <View
+            style={{
+              height: '100%',
+              width: '30%',
+              // backgroundColor: 'green',
+              justifyContent: 'center',
+            }}>
+            <Text>Age: {item.age}</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            height: '50%',
+            width: '100%',
+            // backgroundColor: '#a81',
+            justifyContent: 'center',
+            paddingLeft: h('2%'),
+          }}>
+          <Text>Phone: {item.phone}</Text>
+        </View>
+      </View>
+    </View>
+  );
+
+  separator = () => (
+    <View
+      style={{
+        height: h('1%'),
+      }}
+    />
+  );
 
   render() {
     return (
@@ -17,65 +178,20 @@ export class List extends React.Component {
         style={{
           flex: 1,
         }}>
-        <NavHeader title={'List'} />
+        <NavHeader
+          title={'List'}
+          rightIc={'ios-arrow-forward'}
+          rightPressed={() => {
+            this.setState({loggedIn: !this.state.loggedIn});
+          }}
+        />
         <FlatList
           style={{
-            marginTop: h('5'),
             margin: h('1'),
           }}
-          data={[
-            {
-              name: 'Ali',
-              fName: 'Umer',
-            },
-            {
-              name: 'Imran',
-              fName: 'Kashif',
-            },
-            {
-              name: 'Study',
-              fName: 'Umer',
-            },
-            {
-              name: 'Umer',
-              fName: 'Kashif',
-            },
-            {
-              name: 'Study',
-              fName: 'Umer',
-            },
-            {
-              name: 'Umer',
-              fName: 'Kashif',
-            },
-          ]}
-          renderItem={({item}) => (
-            <View
-              style={{
-                height: h('8%'),
-                // backgroundColor: '#aaf',
-                alignItems: 'center',
-                justifyContent: 'space-evenly',
-                flexDirection: 'row',
-                borderWidth: h('0.1%'),
-                borderRadius: h('1%'),
-              }}>
-              <Text
-                style={{
-                  fontSize: h('3%'),
-                }}>
-                Name: {item.name}
-              </Text>
-              <Text>Father Name: {item.fName}</Text>
-            </View>
-          )}
-          ItemSeparatorComponent={() => (
-            <View
-              style={{
-                height: h('1%'),
-              }}
-            />
-          )}
+          data={this.state.data}
+          renderItem={({item}) => this.renderDesign(item)}
+          ItemSeparatorComponent={() => this.separator()}
           keyExtractor={(index, item) => item.toString()}
         />
       </View>
